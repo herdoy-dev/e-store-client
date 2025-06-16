dcee52a16da71dbbd35b960ba48687c5dcb2352f
= <=>
2025-06-16 22:31:16 +0600

    Update config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.NEXT_PUBLIC_API_BASE_URL || "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization",
+          },
+          { key: "Vary", value: "Origin" },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
